98bb6925a6bd099bc59c97bda46eeabc31688ec3
tomit4 <mosssap@gmail.com>
2024-04-17 02:46:16 -0700

    :construction: Added minigrep project from the book

---
+++ chapter_12_building_a_command_line_program/minigrep/src/lib.rs
@@ -0,0 +1,119 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query: String = args[1].clone();
+        let filename: String = args[2].clone();
+
+        let key: &str = "CASE_INSENSITIVE";
+
+        // provided by ChatGPT as alternative to clippy's match warning:
+        let case_sensitive: bool = if let Ok(val) = env::var(key) {
+            if let Ok(val) = val.parse::<i32>() {
+                val > 0
+            } else {
+                false
+            }
+        } else {
+            false
+        };
+
+        /* My version of the above that causes clippy to throw a warning:
+        let case_sensitive: bool = false;
+        match env::var(key) {
+            Ok(val) => {
+                let val: i32 = val.parse().unwrap();
+                if val > 0 {
+                    case_sensitive = true;
+                }
+            }
+            _ => (),
+        };
+        */
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents: String = fs::read_to_string(config.filename)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&'a str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query: String = query.to_lowercase();
+    let mut results: Vec<&'a str> = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        )
+    }
+}
---
+++ chapter_12_building_a_command_line_program/minigrep/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // NOTE: use eprintln! instead of println! to ensure
+        // you cannot redirect via stdout (i.e.
+        // you cannot use the '>' operator in bash to output
+        // to a file like output.txt because this
+        // instead sends the message to stderr)
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
